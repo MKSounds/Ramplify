35292095dbf63a4d11515a487286f55180bf0114
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-11 14:52:22 +0200

    Delete rename_nodered_flows.sh

--- rename_nodered_flows.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-sudo mv /home/pi/.node-red/flows_guenther.json /home/pi/.node-red/flows_Ramplify.json
-sudo mv /home/pi/.node-red/flows_guenther_cred.json /home/pi/.node-red/flows_Ramplify_cred.json
-#endoffile
