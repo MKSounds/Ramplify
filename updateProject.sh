8aae14c881b770dafffac27ceccfd5a86a9d5e09
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-12 12:23:48 +0200

    Update updateProject.sh
    add node-red install

--- updateProject.sh
+++ updateProject.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+//install bluetooth AAC and aptX codecs
 cd Bluetooth
 
 chmod +755 install.sh
 
 sudo ./install.sh
+
+//instal node-red and dashboard
+cd NodeRed
+
+chmod +755 install-node-red.sh
+
+sudo ./install-node-red.sh
