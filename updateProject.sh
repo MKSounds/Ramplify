23b06e33b42a3b5fc9b83a250e491f14af74f275
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-12 14:14:20 +0200

    Update updateProject.sh
    add install-shairport.sh

--- updateProject.sh
+++ updateProject.sh
@@ -2,23 +2,20 @@
 
 //install bluetooth AAC and aptX codecs
 cd ~/Rmaplify/Bluetooth
-
 chmod +755 install.sh
-
 sudo ./install.sh
-
 
 //install node-red and dashboard
 cd ~/Ramplify/NodeRed
-
 chmod +755 install-node-red.sh
-
 sudo ./install-node-red.sh
-
 
 //install Spotify
 cd ~/Ramplify
-
 chmod +755 install-spotify.sh
-
 sudo ./install-spotify.sh
+
+//install Airplay
+cd ~/Ramplify
+chmod +755 install-shairport.sh
+sudo ./install-shairport.sh
