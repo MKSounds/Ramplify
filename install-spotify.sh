ce3d7717f41354b99fbe417cc6f9eeecb3b70b73
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-12 13:54:14 +0200

    Create install-spotify.sh
    create Spotify install script

---
+++ install-spotify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+//install Spotify
+sudo apt install curl apt-transport-https -y
+sudo curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
+sudo echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
+sudo apt update
+sudo apt install raspotify -y
+
+//you find configs by /etc/default/raspotify
+
+//enable autostart
+sudo systemctl enable raspotify
