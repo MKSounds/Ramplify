08f4e9e510bef7ab4c0ac5c5777e356014bea16a
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-25 14:44:07 +0200

    Update updateProject.sh
    add reboot

--- updateProject.sh
+++ updateProject.sh
@@ -3,33 +3,33 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 #install bluetooth AAC and aptX codecs
-cd ~/Ramplify/Bluetooth
-chmod +755 install.sh
+cd Bluetooth
+sudo chmod +755 install.sh
 sudo ./install.sh
 
 #install node-red and dashboard
 cd ~/Ramplify/NodeRed
-chmod +755 install-node-red.sh
+sudo chmod +755 install-node-red.sh
 sudo ./install-node-red.sh
 
 #install Spotify
 cd ~/Ramplify
-chmod +755 install-spotify.sh
+sudo chmod +755 install-spotify.sh
 sudo ./install-spotify.sh
 
 #install Airplay
 cd ~/Ramplify
-chmod +755 install-shairport.sh
+sudo chmod +755 install-shairport.sh
 sudo ./install-shairport.sh
 
 #install Upnp
 cd ~/Ramplify
-chmod +755 install-Upnp.sh
+sudo chmod +755 install-Upnp.sh
 sudo ./install-Upnp.sh
 
 #install access point
 cd ~Ramplfy/AccessPoint
-chmod +755 install-access-point.sh
+sudo chmod +755 install-access-point.sh
 sudo ./install-access-point.sh
 
 #delete unnecessary project files
@@ -37,3 +37,4 @@ cd ~
 sudo rm -r Ramplify
 
 read -p Installation has finished, press enter to reboot.[ok]
+sudo reboot
