ebaa63c391c93ea56914a4bc0565282691a5f500
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-25 12:23:46 +0200

    Update updateProject.sh
    correct commands

--- updateProject.sh
+++ updateProject.sh
@@ -2,37 +2,37 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-//install bluetooth AAC and aptX codecs
-cd ~/Rmaplify/Bluetooth
+#install bluetooth AAC and aptX codecs
+cd ~/Ramplify/Bluetooth
 chmod +755 install.sh
 sudo ./install.sh
 
-//install node-red and dashboard
+#install node-red and dashboard
 cd ~/Ramplify/NodeRed
 chmod +755 install-node-red.sh
 sudo ./install-node-red.sh
 
-//install Spotify
+#install Spotify
 cd ~/Ramplify
 chmod +755 install-spotify.sh
 sudo ./install-spotify.sh
 
-//install Airplay
+#install Airplay
 cd ~/Ramplify
 chmod +755 install-shairport.sh
 sudo ./install-shairport.sh
 
-//install Upnp
+#install Upnp
 cd ~/Ramplify
 chmod +755 install-Upnp.sh
 sudo ./install-Upnp.sh
 
-//install access point
+#install access point
 cd ~Ramplfy/AccessPoint
 chmod +755 install-access-point.sh
 sudo ./install-access-point.sh
 
-//delete unnecessary project files
+#delete unnecessary project files
 cd ~
 sudo rm -r Ramplify
 
