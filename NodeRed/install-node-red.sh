754127b5aa7a522a74eb635d989be83518e6d3b5
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-25 15:12:52 +0200

    Update install-node-red.sh
    adapt navigate in bash file
    change comments

--- NodeRed/install-node-red.sh
+++ NodeRed/install-node-red.sh
@@ -2,22 +2,36 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-//install latest version of node-red
+#install latest version of node-red
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
-//install dashboard
-cd ~/.node-red
+#install dashboard
+#cd ~/.node-red
+cd ..
+cd ..
+cd .node-red
+
 npm i node-red-dashboard
 
-//clone dashboard flows
-cd ~/Ramplify/NodeRed
+#clone dashboard flows
+#cd ~/Ramplify/NodeRed
+cd ..
+cd Ramplify
+cd NodeRed
+
 sudo mv change_hotspot_password.sh context flows_raspberrypi_cred.json flows_raspberrypi.json package.json package-lock.json rename_nodered_flows.sh settings.js /home/pi/.node-red
 
-cd ~/.node-red
+#cd ~/.node-red
+cd ..
+cd ..
+cd .node-red
 chmod +755 change_hotspot_password.sh
 chmod +755 rename_nodered_flows.sh
 
-//enable autostart
+cd ..
+cd ..Ramplify
+
+#enable autostart
 sudo systemctl enable nodered.service
 
 sudo node-red-start
