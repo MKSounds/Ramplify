b703cb5fe4659e24de90020c9a648b61444d95be
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-25 19:54:44 +0200

    Update install-node-red.sh
    delete if for root

--- NodeRed/install-node-red.sh
+++ NodeRed/install-node-red.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-
 #install latest version of node-red
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
