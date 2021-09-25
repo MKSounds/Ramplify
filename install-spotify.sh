d21e78c0b8f13ababe9639e4b8b81817387c2838
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-09-25 17:37:36 +0200

    Update install-spotify.sh
    adapt comments

--- install-spotify.sh
+++ install-spotify.sh
@@ -2,14 +2,14 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-//install Spotify
+#install Spotify
 sudo apt install curl apt-transport-https -y
 sudo curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
 sudo echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
 sudo apt update
 sudo apt install raspotify -y
 
-//you find configs by /etc/default/raspotify
+#you find configs by /etc/default/raspotify
 
-//enable autostart
+#enable autostart
 sudo systemctl enable raspotify
