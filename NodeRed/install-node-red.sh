3f2cea8430884ae52f09e0b77df23cb670acd726
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-10-02 10:28:10 +0200

    Create install-node-red.sh
    add install-node-red.sh

---
+++ NodeRed/install-node-red.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+#install latest version of node-red
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+
+#install dashboard
+cd ~/.node-red
+cd .node-red
+
+npm i node-red-dashboard
+
+#clone dashboard flows
+cd ~/Ramplify/NodeRed
+
+sudo mv change_hotspot_password.sh context flows_raspberrypi_cred.json flows_raspberrypi.json package.json package-lock.json rename_nodered_flows.sh settings.js /home/pi/.node-red
+
+cd ~/.node-red
+chmod +755 change_hotspot_password.sh
+chmod +755 rename_nodered_flows.sh
+
+cd ~/Ramplify/NodeRed
+
+#enable autostart
+sudo systemctl enable nodered.service
+
+#sudo node-red-start
