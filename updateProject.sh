da715393dd1f97c69721c7460dcecd83002dc746
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-10-02 12:27:58 +0200

    Update updateProject.sh
    delete if not root
    do not run it as root! Node Red won't work
    change to absolute path

--- updateProject.sh
+++ updateProject.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+#if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 #install bluetooth AAC and aptX codecs
 #cd Bluetooth
@@ -11,12 +11,12 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 #cd ..
 
 #install node-red and dashboard
-cd NodeRed
+cd ~/Ramplify/NodeRed
 sudo chmod +755 install-node-red.sh
 bash install-node-red.sh
 echo Node-Red installed
 
-cd ..
+cd ~/Ramplify
 
 #install Spotify
 sudo chmod +755 install-spotify.sh
@@ -34,16 +34,17 @@ sudo ./install-Upnp.sh
 echo Upnp installed
 
 #install access point
-cd AccessPoint
+#cd AccessPoint
+cd ~/Ramplify/AccessPoint
 sudo chmod +755 install-access-point.sh
 sudo ./install-accespoint.sh
 echo AccessPoint installed
 
-cd ..
+cd ~
 
 #delete unnecessary project files
 sudo rm -r Ramplify
 echo Remplify removed
 
-read -p Installation has finished, press enter to reboot.[ok]
-sudo reboot
+#read -p Installation has finished, press enter to reboot.[ok]
+#sudo reboot
