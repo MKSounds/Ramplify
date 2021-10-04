5da789d56b185426a10eea709dd18d579a940902
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-10-04 22:52:53 +0200

    Update install-accespoint.sh

--- AccessPoint/install-accespoint.sh
+++ AccessPoint/install-accespoint.sh
@@ -2,26 +2,27 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-cd ..
-cd ..
+cd ~
+#cd ..
+#cd ..
 
 sudo apt-get install hostapd wpasupplicant dnsmasq
 
 sudo update-rc.d -f hostapd remove
 sudo update-rc.d -f dnsmasq remove
 
-echo Now enable the networking at boot. After the configs are
-echo opened move to 'System Options' and select 'Network at boot'.
-echo Press enter and confirm with 'yes' for enabling.
-read -p Press enter to continue after reading
+echo "Now enable the networking at boot. After the configs are"
+echo "opened move to 'System Options' and select 'Network at boot'."
+echo "Press enter and confirm with 'yes' for enabling."
+read -p "Press enter to continue after reading"
 
 sudo raspi-config
 
 #replace DAEMON_CONF="" in /etc/default/hostapd
 sudo sed -i 's$#DAEMON_CONF=""$DAEMON_CONF="/etc/hostapd/hostapd.conf"$' /etc/default/hostapd
 
-cd Ramplify
-cd AccessPoint
+#cd Ramplify
+cd ~Ramplify/AccessPoint
 
 sudo mv hostapd.conf /etc/hostapd
 
@@ -29,4 +30,4 @@ sudo mv dnsmasq.conf /etc
 
 sudo mv rc.local /etc/rc.local
 
-cd ..
+#cd ..
