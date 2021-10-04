89ef830749fe1973bfef07eeee13e2edaaadc51b
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-10-04 22:45:42 +0200

    Update install-accespoint.sh

--- AccessPoint/install-accespoint.sh
+++ AccessPoint/install-accespoint.sh
@@ -13,7 +13,7 @@ sudo update-rc.d -f dnsmasq remove
 echo Now enable the networking at boot. After the configs are
 echo opened move to 'System Options' and select 'Network at boot'.
 echo Press enter and confirm with 'yes' for enabling.
-read -p Press enter to continue after reading.[ok]
+read -p Press enter to continue after reading
 
 sudo raspi-config
 
