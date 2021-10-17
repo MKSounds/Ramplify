d25a6aac33254a390cac8b250c37013a52586d9b
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-10-17 18:10:12 +0200

    Create GenericI2S-Traiber-install.sh
    create file to add GenericI2S-Traiber

---
+++ GenericI2S-Traiber-install.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+cd ~
+git clone https://github.com/MKSounds/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi.git
+
+cd ~/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi
+sudo mv generic_audio_out_i2s_master.dtbo generic_audio_out_i2s_slave.dtbo generic_audio_out_tdm8_master.dtbo generic_audio_out_tdm8_slave.dtbo /boot/overlays
+
+cd ~
+sudo rm -r ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi
