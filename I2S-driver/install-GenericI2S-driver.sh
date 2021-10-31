053f6b109b0c9984d821cc95c52c4eb4ea9a359b
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-10-31 11:47:32 +0100

    Update install-GenericI2S-driver.sh

--- I2S-driver/install-GenericI2S-driver.sh
+++ I2S-driver/install-GenericI2S-driver.sh
@@ -8,7 +8,7 @@ git clone https://github.com/MKSounds/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi
 
 sudo cp ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi/adau1701-i2s.dtbo /boot/overlays
 
-sudo sed -i 's$dtparam=audio=on$#dtparam=audio=on$' /etc/hostname
+sudo sed -i 's$dtparam=audio=on$#dtparam=audio=on$' /boot/config.txt
 
 sed -i '$ a\'"$dtoverlay=adau1701-i2s" $config.txt /boot
 
