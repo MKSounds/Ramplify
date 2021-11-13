1273acf092bd6ed7c4d6cd3a23d401423b84e2c4
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-11-13 16:23:21 +0100

    Update install-GenericI2S-driver.sh
    change install

--- I2S-driver/install-GenericI2S-driver.sh
+++ I2S-driver/install-GenericI2S-driver.sh
@@ -4,15 +4,21 @@ cd ~
 
 sudo rpi-update 5c80565
 
+#git clone https://github.com/MKSounds/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi.git
+
 git clone https://github.com/MKSounds/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi.git
 
-sudo cp ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi/adau1701-i2s.dtbo /boot/overlays
+sudo mv generic_audio_out_i2s_master.dtbo generic_audio_out_i2s_slave.dtbo generic_audio_out_tdm8_master.dtbo generic_audio_out_tdm8_slave.dtbo /boot/overlays
 
-sudo sed -i 's$dtparam=audio=on$#dtparam=audio=on$' /boot/config.txt
+#sudo cp ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi/adau1701-i2s.dtbo /boot/overlays
 
-sed -i '$ a\'"$dtoverlay=adau1701-i2s" $config.txt /boot
+#sudo sed -i 's$dtparam=audio=on$#dtparam=audio=on$' /boot/config.txt
 
-cd ~/Ramplify/I2S-driver
+#sed -i '$ a\'"$dtoverlay=adau1701-i2s" $config.txt /boot
+
+sed -i '$ a\'"$[all]" $config.txt /boot
+
+cd /home/pi/Ramplify/I2S-driver
 
 sudo mv .asoundrc /home/pi
 
