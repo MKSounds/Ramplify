d35925b04e4a3a50d6ca6db6b461ef819cf21df0
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-11-14 18:19:12 +0100

    Update install-snapcast.sh

--- Snapcast/install-snapcast.sh
+++ Snapcast/install-snapcast.sh
@@ -2,12 +2,12 @@
 
 cd /home/pi/Ramplify/Snapcast
 
-chmod +755 install-snapserver.sh
+sudo chmod +755 install-snapserver.sh
 sudo./install-snapserver.sh
 echo "Snapserver installed"
 
 cd /home/pi/Ramplify/Snapcast
 
-chmod +755 install-snapclient.sh
+sudo chmod +755 install-snapclient.sh
 sudo ./install-snapclient.sh
 echo "Snapclient installed!"
