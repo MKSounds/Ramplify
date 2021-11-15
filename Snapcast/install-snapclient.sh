2a328e94504d2bae9a7f7a59a555db3026bc100c
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-11-15 23:51:36 +0100

    Update install-snapclient.sh

--- Snapcast/install-snapclient.sh
+++ Snapcast/install-snapclient.sh
@@ -3,7 +3,10 @@
 cd /home/pi
 
 #install
-wget https://github.com/badaix/snapcast/releases/download/v0.25.0/snapclient_0.25.0-1_armhf.deb
-sudo dpkg -i snapclient_0.25.0-1_armhf.deb
-rm snapclient_0.25.0-1_armhf.deb
+#wget https://github.com/badaix/snapcast/releases/download/v0.25.0/snapclient_0.25.0-1_armhf.deb
+#sudo dpkg -i snapclient_0.25.0-1_armhf.deb
+#rm snapclient_0.25.0-1_armhf.deb
+wget  https://github.com/badaix/snapcast/releases/download/v0.10.0/snapclient_0.10.0_armhf.deb
+sudo dpkg -i snapclient_0.10.0_armhf.deb
+rm snapclient_0.10.0_armhf.deb
 echo "Snapclient installed!"
