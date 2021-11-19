6a58e0724641219e1f113ad22b5da7c4e0cbd1e5
Tim-Keicher <61784648+Tim-Keicher@users.noreply.github.com>
2021-11-19 20:56:11 +0100

    Update install-portmapping.sh

--- install-portmapping.sh
+++ install-portmapping.sh
@@ -3,7 +3,7 @@
 cd /home/pi
 
 sudo apt install iptables-persistent
-sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT --YES
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1880
 sudo netfilter-persistent save
 sudo netfilter-persistent reload
