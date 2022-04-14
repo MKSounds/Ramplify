9f19eac1ab31c94863db829740534838930c8109
MKSounds <34724166+MKSounds@users.noreply.github.com>
2022-04-14 13:17:41 +0200

    Update install-GenericI2S-driver.sh
    - deleted rpi-update
    - changed mv to cp for overlay files
    - added sudo for sed, changed  sed -i '$ a\a\'"[all]"         /boot/config.txt
    - uncommented asoundrc (src)

--- I2S-driver/install-GenericI2S-driver.sh
+++ I2S-driver/install-GenericI2S-driver.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
+#sudo rpi-update 5c80565
+
 cd /home/pi
-
-sudo rpi-update 5c80565
-
-#git clone https://github.com/MKSounds/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi.git
-
 git clone https://github.com/MKSounds/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi.git
 
-sudo mv generic_audio_out_i2s_master.dtbo generic_audio_out_i2s_slave.dtbo generic_audio_out_tdm8_master.dtbo generic_audio_out_tdm8_slave.dtbo /boot/overlays
-
-#sudo cp ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi/adau1701-i2s.dtbo /boot/overlays
-
-#sudo sed -i 's$dtparam=audio=on$#dtparam=audio=on$' /boot/config.txt
-
-#sed -i '$ a\'"$dtoverlay=adau1701-i2s" $config.txt /boot
+cd /home/pi/ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi
+sudo cp generic_audio_out_i2s_master.dtbo   /boot/overlays
+sudo cp generic_audio_out_i2s_slave.dtbo    /boot/overlays
+sudo cp generic_audio_out_tdm8_master.dtbo  /boot/overlays
+sudo cp generic_audio_out_tdm8_slave.dtbo   /boot/overlays
 
 #add lines at the end of the file
-sed -i '$ a\a\'"[all]" /boot/config.txt
-sed -i '$ a\'"#placeholder" /boot/config.txt
-sed -i '$ a\'"#placeholder" /boot/config.txt
+sudo sed -i '$ a\'"[all]"         /boot/config.txt
+sudo sed -i '$ a\'"#placeholder"  /boot/config.txt
+sudo sed -i '$ a\'"#placeholder"  /boot/config.txt
 
-cd /home/pi/Ramplify/I2S-driver
-
-sudo mv .asoundrc /home/pi
+#cd /home/pi/Ramplify/I2S-driver
+#sudo mv .asoundrc /home/pi
 
 cd /home/pi
+sudo rm -r ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi       #-r for removing filled directories
 
-sudo remove -r ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi
