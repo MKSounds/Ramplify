5dc82ed91d4a174188cad15948bb1c828129cb97
MKSounds <34724166+MKSounds@users.noreply.github.com>
2022-04-14 17:37:23 +0200

    Update install-snapclient.sh

--- Snapcast/install-snapclient.sh
+++ Snapcast/install-snapclient.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-cd /home/pi
 
-#snapclient v0.24 needs dependency libpulse0
-
-#install v24
-#wget  https://github.com/badaix/snapcast/releases/download/v0.24.0/snapclient_0.24.0-1_armhf.deb
-#sudo dpkg -i snapclient_0.24.0-1_armhf.deb
-#rm snapclient_0.24.0-1_armhf.deb
-
-#install v25
-#wget https://github.com/badaix/snapcast/releases/download/v0.25.0/snapclient_0.25.0-1_armhf.deb
-#sudo dpkg -i snapclient_0.25.0-1_armhf.deb
-#rm snapclient_0.25.0-1_armhf.deb
-
-#install v26
-wget https://github.com/badaix/snapcast/releases/download/v0.25.0/snapclient_0.25.0-1_armhf.deb
-sudo dpkg -i snapclient_0.25.0-1_armhf.deb
-rm snapclient_0.25.0-1_armhf.deb
-
-
-echo "Snapclient installed!"
